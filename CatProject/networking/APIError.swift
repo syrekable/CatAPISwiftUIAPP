73041eb0ae95c73288c2fb373d572c9850ddfe27
Jordan Niedzielski <contact@syrekable.tech>
2022-08-18 16:43:06 +0200

    custom error messages for user

--- CatProject/networking/APIError.swift
+++ CatProject/networking/APIError.swift
@@ -9,14 +9,21 @@ import Foundation
 
 enum APIError: Error, CustomStringConvertible {
     case badURL
-    case response(statusCode: Int)
+    case badResponse(statusCode: Int)
     case url(URLError)
     case parsing
     case unknown
     
     var localizedDescription: String {
         // feedback for user
-        ""
+        switch self {
+        case .badURL, .parsing, .unknown:
+            return "Sorry, something went wrong"
+        case .badResponse(_):
+            return "Sorry, the connection to server failed."
+        case .url(let error):
+            return error.localizedDescription ?? "Something went wrong"
+        }
     }
     
     var description: String {
