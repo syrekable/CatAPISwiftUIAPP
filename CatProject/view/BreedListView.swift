d87395245d1dd47e171d1dfeb027c10c3cc2da2a
Jordan Niedzielski <contact@syrekable.tech>
2022-08-18 15:29:24 +0200

    breed list view

--- CatProject/view/BreedListView.swift
+++ CatProject/view/BreedListView.swift
@@ -8,13 +8,14 @@
 import SwiftUI
 
 struct BreedListView: View {
+    let breeds: [Breed]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Showing \(breeds.count) breeds.")
     }
 }
 
 struct BreedListView_Previews: PreviewProvider {
     static var previews: some View {
-        BreedListView()
+        BreedListView(breeds: [Breed]())
     }
 }
