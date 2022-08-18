0e5d34501b7da368bff1e3e12d64a5850525961f
Jordan Niedzielski <contact@syrekable.tech>
2022-08-18 13:32:08 +0200

    Initial Commit

---
+++ CatProject/CatProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatProjectApp.swift
+//  CatProject
+//
+//  Created by Jordan Niedzielski on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CatProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
