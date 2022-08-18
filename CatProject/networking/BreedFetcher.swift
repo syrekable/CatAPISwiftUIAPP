c2b1e9b3d4fcf181a38a7052705199146cd1f4a0
Jordan Niedzielski <contact@syrekable.tech>
2022-08-18 15:11:08 +0200

    initial data fetcher

--- CatProject/networking/BreedFetcher.swift
+++ CatProject/networking/BreedFetcher.swift
@@ -13,4 +13,19 @@ class BreedFetcher: ObservableObject {
     init() {
         
     }
+    
+    func fetchAllBreeds() {
+        // TODO: handle bad url error
+        let breedsUrl = URL(string: "https://api.thecatapi.com/v1/breeds")!
+        let task = URLSession.shared.dataTask(with: breedsUrl) { data, response, error in
+            if let data = data {
+                do {
+                    self.breeds = try JSONDecoder().decode([Breed].self, from: data)
+                } catch {
+                    // TODO: show error in ui
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
 }
