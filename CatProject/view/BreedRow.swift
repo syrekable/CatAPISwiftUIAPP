fde5abdc15ea8c91cc234180b385087de05edda8
Jordan Niedzielski <contact@syrekable.tech>
2022-08-19 14:50:57 +0200

    initial Breed's image

--- CatProject/view/BreedRow.swift
+++ CatProject/view/BreedRow.swift
@@ -11,6 +11,17 @@ struct BreedRow: View {
     let breed: Breed
     var body: some View {
         HStack {
+            if breed.image != nil {
+                AsyncImage(url: URL(string: breed.image!.url!)) { phase in
+                    if let image = phase.image {
+                        image // Displays the loaded image.
+                    } else if phase.error != nil {
+                        Color.red // Indicates an error.
+                    } else {
+                        Color.blue // Acts as a placeholder.
+                    }
+                }
+            }
             VStack(alignment: .leading, spacing: 5) {
                 Text(breed.name)
                 Text(breed.temperament)
