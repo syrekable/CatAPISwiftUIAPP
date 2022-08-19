cb615caf06f82ac5bc8c186b55c7c4b5277a62e8
Jordan Niedzielski <contact@syrekable.tech>
2022-08-19 14:54:07 +0200

    improved BreedRow UI

--- CatProject/view/BreedRow.swift
+++ CatProject/view/BreedRow.swift
@@ -9,18 +9,23 @@ import SwiftUI
 
 struct BreedRow: View {
     let breed: Breed
+    let imageSize: CGFloat = 100
     var body: some View {
         HStack {
             if breed.image != nil {
                 AsyncImage(url: URL(string: breed.image!.url!)) { phase in
                     if let image = phase.image {
-                        image // Displays the loaded image.
+                        image
+                            .resizable()
+                            .frame(width: imageSize, height: imageSize)
                     } else if phase.error != nil {
-                        Color.red // Indicates an error.
+                        Color.red.frame(width: imageSize, height: imageSize) // Indicates an error.
                     } else {
-                        Color.blue // Acts as a placeholder.
+                        ProgressView() // Acts as a placeholder.
                     }
                 }
+            } else {
+                Color.gray.frame(width: imageSize, height: imageSize)
             }
             VStack(alignment: .leading, spacing: 5) {
                 Text(breed.name)
