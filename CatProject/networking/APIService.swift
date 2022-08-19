4235595f6e5ed5d47ef4565e7afc7030265d1dbf
Jordan Niedzielski <contact@syrekable.tech>
2022-08-19 12:01:01 +0200

    generic fetcher

--- CatProject/networking/APIService.swift
+++ CatProject/networking/APIService.swift
@@ -20,16 +20,40 @@ struct APIService {
              // there is a system error, ex. no internet
              if let error = error {
                  completion(.failure(.url(error as! URLError)))
-             } else if let response = response as? HTTPURLResponse {
+             } else if let response = response as? HTTPURLResponse, !(200...299).contains(response.statusCode) {
                  // the response is invalid
-                !(200...299).contains(response.statusCode)
-                 completion(.failure(.badResponse(statusCode: response.statusCode)))
+                     completion(.failure(.badResponse(statusCode: response.statusCode)))
             } else if let data = data {
                 do {
                     let breeds = try JSONDecoder().decode([Breed].self, from: data)
                     completion(.success(breeds))
                 } catch {
-                    completion(.failure(.parsing(error as! DecodingError)))
+                    completion(.failure(.parsing(error as? DecodingError)))
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    /// Fetches data from a URL and decodes it into any Decodable type.
+    /// Features error handling for system, url, status code and decoding errors.
+    func fetch<T: Decodable>(url: URL?, completion: @escaping (Result<T, APIError>) -> Void) {
+        guard let url = url else {
+            completion(.failure(.badURL))
+            return
+        }
+        
+         let task = URLSession.shared.dataTask(with: url) {data, response, error in
+             if let error = error {
+                 completion(.failure(.url(error as! URLError)))
+             } else if let response = response as? HTTPURLResponse, !(200...299).contains(response.statusCode) {
+                     completion(.failure(.badResponse(statusCode: response.statusCode)))
+            } else if let data = data {
+                do {
+                    let result = try JSONDecoder().decode(T.self, from: data)
+                    completion(.success(result))
+                } catch {
+                    completion(.failure(.parsing(error as? DecodingError)))
                 }
             }
         }
